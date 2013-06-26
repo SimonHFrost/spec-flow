6af42ef5bc0fb385d4bf1bcd340bdfa79807739c
Simon Frost <simonhfrost@gmail.com>
2013-06-26 22:10:37 +1200

    Concept steps for api

---
+++ SpecFlowTest/ApiFiles/ApiTestingSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTest.ApiFiles
+{
+    [Binding]
+    public class ApiTestingSteps
+    {
+        [Given(@"I am logged in as customer '(.*)'")]
+        public void GivenIAmLoggedInAsCustomer(string login)
+        {
+            // AccountManager.Login(login, GetPassword(login));
+        }
+        
+        [Given(@"I have (.*) of my customers ready to import")]
+        public void GivenIHaveOfMyCustomersReadyToImport(int numCustomers)
+        {
+            // _rows = CsvManager.GetRowsForUser(_currentLogin, numCustomers);
+        }
+        
+        [When(@"I import these customers")]
+        public void WhenIImportTheseCustomers()
+        {
+            // ApiImportCustomers("import", _rows);
+        }
+        
+        [Then(@"I want to see them displayed in Route")]
+        public void ThenIWantToSeeThemDisplayedInRoute()
+        {
+            // GetAllCustomers(_currentLogin);
+        }
+    }
+}
